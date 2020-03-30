1a7ec149f6258308b8212b4364b2309a869a4935
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-03-31 00:51:04 +0300

    Improve the WeaverConfig class Start adding documentation and JetBrains annotations.

--- Sigourney/Weaver.cs
+++ Sigourney/Weaver.cs
@@ -5,18 +5,29 @@
 
 using System.IO;
 using System.Reflection;
+using JetBrains.Annotations;
 using Mono.Cecil;
 using Serilog;
 using Serilog.Core;
 
 namespace Sigourney
 {
+    /// <summary>
+    /// An abstract class for a process that modifies (weaves) a .NET assembly.
+    /// </summary>
+    /// <remarks>Yes, I know about the type's fully qualified name...</remarks>
+    [PublicAPI]
     public abstract partial class Weaver
     {
         protected readonly ILogger Log;
         private readonly string _version;
 
-        public Weaver(ILogger? log = null)
+        /// <summary>
+        /// Creates a <see cref="Weaver"/>.
+        /// </summary>
+        /// <param name="log">A Serilog <see cref="ILogger"/> that
+        /// will record any events inside the weaver.</param>
+        protected Weaver(ILogger? log = null)
         {
             Log = log ?? Logger.None;
             var ownerAssembly = GetType().Assembly;
@@ -24,17 +35,32 @@ namespace Sigourney
                 ?.InformationalVersion ?? ownerAssembly.GetName().Version.ToString();
         }
 
+        /// <summary>
+        /// Weaves an assembly.
+        /// </summary>
+        /// <remarks>If weaving succeeds, a static class named "ProcessedBy<see cref="ProductName"/>"
+        /// will be presented to instruct Sigourney not to weave it again.</remarks>
+        /// <param name="inputPath">The path of the assembly to weave.</param>
+        /// <param name="outputPath">The path where the weaved assembly will be stored.
+        /// Defaults to <paramref name="inputPath"/>.</param>
+        /// <param name="config">An optional <see cref="WeaverConfig"/> object to
+        /// further parameterize the weaving process.</param>
         public void Weave(string inputPath, string? outputPath = null, WeaverConfig? config = null)
         {
             using var resultingAsembly = new MemoryStream();
             using (var asm = AssemblyDefinition.ReadAssembly(inputPath))
             {
+                var assemblyName = asm.Name.Name;
                 if (config != null)
                     FindStrongNameKey(config, asm);
 
                 if (AssemblyMarker.ShouldProcess(asm, ProductName))
                 {
-                    if (!DoWeave(asm)) return;
+                    if (!DoWeave(asm, config))
+                    {
+                        Log.Debug("Skipping weaving {AssemblyName} as requested.", assemblyName);
+                        return;
+                    }
 
                     AssemblyMarker.MarkAsProcessed(asm, ProductName, _version, Log);
                     var writerParams = new WriterParameters
@@ -44,6 +70,10 @@ namespace Sigourney
                     asm.Name.PublicKey = _publicKey;
                     asm.Write(resultingAsembly, writerParams);
                 }
+                else
+                {
+                    Log.Debug("{AssemblyName} is already weaved.", assemblyName);
+                }
             }
 
             using var outputFile = File.Create(outputPath ?? inputPath);
@@ -51,7 +81,19 @@ namespace Sigourney
             resultingAsembly.CopyTo(outputFile);
         }
 
-        protected abstract string ProductName { get; }
-        protected abstract bool DoWeave(AssemblyDefinition asm);
+        /// <summary>
+        /// Gets an informative name of the weaver.
+        /// </summary>
+        /// <remarks>Defaults to the name of the assembly
+        /// the weaver's class was defined.</remarks>
+        protected virtual string ProductName => GetType().Assembly.GetName().Name;
+
+        /// <summary>
+        /// Performs the actual weaving using Mono.Cecil.
+        /// </summary>
+        /// <param name="asm">The assembly to modify in an <see cref="AssemblyDefinition"/> format.</param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        protected abstract bool DoWeave(AssemblyDefinition asm, WeaverConfig? config);
     }
 }
--- Sigourney/WeaverConfig.cs
+++ Sigourney/WeaverConfig.cs
@@ -3,17 +3,66 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Microsoft.Build.Framework;
+
 namespace Sigourney
 {
+    /// <summary>
+    /// A class containing configuration data for a <see cref="Weaver"/>.
+    /// </summary>
+    /// <remarks>The necessary ones are typically provided by MSBuild,
+    /// but custom applications can give their own.</remarks>
+    [PublicAPI]
     public class WeaverConfig
     {
-        public string? IntermediateDirectory { get; set; }
-        public string? KeyOriginatorFile { get; set; }
+        private string? _intermediateDirectory;
 
-        public string? AssemblyOriginatorKeyFile { get; set; }
+        private bool _signAssembly;
 
-        public string? KeyFilePath => KeyOriginatorFile ?? AssemblyOriginatorKeyFile;
+        public string? KeyFilePath { get; set; }
 
-        public bool SignAssembly { get; set; } = false;
+        public bool SignAssembly
+        {
+            get => _signAssembly;
+            set => _signAssembly = value;
+        }
+
+        public string? IntermediateDirectory
+        {
+            get => _intermediateDirectory;
+            set => _intermediateDirectory = value;
+        }
+
+        public readonly IReadOnlyDictionary<string, string> AllConfiguration;
+
+        private void Init()
+        {
+            AllConfiguration.TryGetValue(nameof(IntermediateDirectory), out _intermediateDirectory);
+            AllConfiguration.TryGetValue("KeyOriginatorFile", out var kof);
+            AllConfiguration.TryGetValue("AssemblyOriginatorKeyFile", out var aokf);
+            KeyFilePath = kof ?? aokf;
+            if (AllConfiguration.TryGetValue(nameof(SignAssembly), out var sa))
+                bool.TryParse(sa, out _signAssembly);
+        }
+
+        public WeaverConfig() : this(new Dictionary<string, string>())
+        {
+        }
+
+        public WeaverConfig(IReadOnlyDictionary<string, string> config)
+        {
+            AllConfiguration = config;
+            Init();
+        }
+
+        public WeaverConfig(IEnumerable<ITaskItem> items)
+        {
+            AllConfiguration = items.SelectMany(x => x.CloneCustomMetadata().Cast<DictionaryEntry>())
+                .ToDictionary(x => x.Key.ToString(), x => x.Value.ToString());
+        }
     }
 }
