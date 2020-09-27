2b2a5e87c23d76ded14b329df53c828b6e1d0c37
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-09-27 21:25:10 +0300

    Write a test weaver.

---
+++ tests/testweaver-1/TestWeaver1.cs
@@ -0,0 +1,15 @@
+﻿using Mono.Cecil;
+using Sigourney;
+
+namespace testweaver_1
+{
+    public class TestWeaver1 : MSBuildWeaver
+    {
+        protected override bool DoWeave(AssemblyDefinition asm)
+        {
+            asm.MainModule.Types.Add(new TypeDefinition("", "TestWeaver1Rulez",
+                TypeAttributes.Class | TypeAttributes.Sealed));
+            return true;
+        }
+    }
+}
