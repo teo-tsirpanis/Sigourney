4868c4cb207ed3302cb77b799b49390eae76f4fd
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-10-06 21:12:54 +0300

    Zip the test logs when they are sent to CI.
    Downloading all of them at once is helpful.

--- test.ps1
+++ test.ps1
@@ -3,9 +3,14 @@
 
 function Test-ExitCode {if ($LASTEXITCODE -ne 0) {exit $LASTEXITCODE}}
 
+$TestLogs = './test-logs/'
 $TestProject = './tests/test.proj'
 
+if (Test-Path $TestLogs -PathType Container) {Remove-Item $TestLogs -Recurse -Force}
+
 for ($i = 1; $i -le 3; $i++) {
-    dotnet msbuild $TestProject /bl:run-$i.binlog
+    dotnet msbuild $TestProject ("/bl:{0}run-{1}.binlog" -f $TestLogs, $i)
     Test-ExitCode
 }
+
+Compress-Archive $TestLogs -DestinationPath "test-logs.zip" -Force
