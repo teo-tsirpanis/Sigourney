43b17f484f1801b9d57de7342d2431111853e0b9
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-10-30 15:07:48 +0200

    Use a local NuGet package folder. At last, local builds are fully reproducible. No more manually deleting the global NuGet cache! 🥳 And run the tests on .NET Framework-based MSBuild (the reason will be revealed at the next commit).

--- test.ps1
+++ test.ps1
@@ -2,14 +2,26 @@
 # It also keeps binary logs of each test run (which are kept as artifacts by AppVeyor).
 
 function Test-ExitCode {if ($LASTEXITCODE -ne 0) {exit $LASTEXITCODE}}
+function DotnetClean {dotnet clean /v:m /nodereuse:false $TestProject; Test-ExitCode}
+function Remove-Directory-Checked {
+    param ([string]$Directory)
+    if (Test-Path $Directory -PathType Container) {Remove-Item $Directory -Recurse -Force}
+}
 
 $TestLogs = './test-logs/'
 $TestProject = './tests/test.proj'
 
-if (Test-Path $TestLogs -PathType Container) {Remove-Item $TestLogs -Recurse -Force}
+Remove-Directory-Checked $TestLogs
 
+DotnetClean
 for ($i = 1; $i -le 3; $i++) {
-    dotnet msbuild $TestProject ("/bl:{0}run-{1}.binlog" -f $TestLogs, $i)
+    dotnet msbuild $TestProject /v:m /nodereuse:false ("/bl:{0}dotnet-{1}.binlog" -f $TestLogs, $i)
+    Test-ExitCode
+}
+
+DotnetClean
+for ($i = 1; $i -le 3; $i++) {
+    msbuild $TestProject /v:m /nodereuse:false ("/bl:{0}msbuild-{1}.binlog" -f $TestLogs, $i)
     Test-ExitCode
 }
 
