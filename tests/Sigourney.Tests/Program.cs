90213909b9aa7d89c2ce08831e3f174ff6278458
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-10-30 21:06:53 +0200

    Reproduce a Mono.Cecil bug that causes errors on .NET Framework-based MSBuild.
    See? That's why we run the tests on two frameworks.

--- tests/Sigourney.Tests/Program.cs
+++ tests/Sigourney.Tests/Program.cs
@@ -4,11 +4,26 @@
 // https://opensource.org/licenses/MIT
 
 using System;
+using System.Diagnostics;
+using static System.Diagnostics.TraceLevel;
 
 namespace Sigourney.Tests
 {
     static class Program
     {
+        // The following line would fail on .NET Framework-based MSBuild.
+        // When writing this constant, Cecil will try to resolve the "netstandard"
+        // assembly. It will fail on .NET Framework-based MSBuild because the .NET
+        // Starnard edition of Cecil (that is always used by Sigourney) will fail
+        // to find the assembly because it will use .NET Core-specific methods.
+        // Cecil's PR 701 will make it use the .NET Framework-specific resolver
+        // on .NET Standard if needed, and PR 702 will omit the resolver for the
+        // enum constant writer.
+        // Until either of the fixes above is shipped, what Sigourney can do is
+        // explicitly pass the assembly references for Cecil to find, which is
+        // good nevertheless.
+        private const TraceLevel ThisLineWillCauseAn = Error;
+
         static int assertCount = 0;
         static int errorCount = 0;
 
