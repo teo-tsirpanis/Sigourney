d755d5352a23d809c8fcb8566e75f9926b829451
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-11-01 13:30:56 +0200

    Zip the test logs even on failed builds. Run the .NET Framework-based msbuild tests only on Windows.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -6,7 +6,9 @@
 * `cd tests`
 * `dotnet msbuild`
 
-The `test.ps1` file runs the tests thrice and leaves MSBuild binary logs for each one.
+The `test.ps1` file runs the tests six times -thrice with `dotnet msbuild` and thrice with `msbuild` itself (only on Windows)- to ensure that projects using Sigourney work incrementally. Each test is made of building a test program that uses two test weavers, and then running it which verifies that it has been weaved. MSBuild binary logs are left for each test run.
+
+Should the test projects fail loading on an IDE, try running it first.
 
 <s>__Important:__ Locally running the tests after a change to Sigourney or one of the test weavers requires removing the packages named `Sigourney`, `testweaver-1` and `testweaver-2` from NuGet's global packages cache (its location can be retrieved with `dotnet nuget locals global-packages -l`). If the packages cannot be removed try closing any IDE and running `dotnet build-server shutdown`.
 
--- test.ps1
+++ test.ps1
@@ -1,28 +1,32 @@
 # This little script runs Sigourney's test project thrice to ensure it works repeatedly.
 # It also keeps binary logs of each test run (which are kept as artifacts by AppVeyor).
 
-function Test-ExitCode {if ($LASTEXITCODE -ne 0) {exit $LASTEXITCODE}}
-function DotnetClean {dotnet clean /v:m /nodereuse:false $TestProject; Test-ExitCode}
+function DotnetClean {dotnet clean /v:m /nodereuse:false $TestProject}
 function Remove-Directory-Checked {
     param ([string]$Directory)
-    if (Test-Path $Directory -PathType Container) {Remove-Item $Directory -Recurse -Force}
+    if (Test-Path $Directory -PathType Container) {
+        Remove-Item $Directory -Recurse -Force
+    }
 }
 
 $TestLogs = './test-logs/'
 $TestProject = './tests/test.proj'
 
 Remove-Directory-Checked $TestLogs
+# dotnet clean might fail the first time.
+Remove-Item tests\**\obj\* -Recurse -Force
 
 DotnetClean
-for ($i = 1; $i -le 3; $i++) {
+for ($i = 1; ($i -le 3) -and ($LASTEXITCODE -eq 0); $i++) {
     dotnet msbuild $TestProject /v:m /nodereuse:false ("/bl:{0}dotnet-{1}.binlog" -f $TestLogs, $i)
-    Test-ExitCode
 }
 
-DotnetClean
-for ($i = 1; $i -le 3; $i++) {
-    msbuild $TestProject /v:m /nodereuse:false ("/bl:{0}msbuild-{1}.binlog" -f $TestLogs, $i)
-    Test-ExitCode
+if ($IsWindows -and ($LASTEXITCODE -eq 0)) {
+    DotnetClean
+    for ($i = 1; ($i -le 3) -and ($LASTEXITCODE -eq 0); $i++) {
+        msbuild $TestProject /v:m /nodereuse:false ("/bl:{0}msbuild-{1}.binlog" -f $TestLogs, $i)
+    }
 }
 
 Compress-Archive $TestLogs -DestinationPath "test-logs.zip" -Force
+exit $LASTEXITCODE
